683af6861f95718dd5627e91c183868facb54b1a
youssef9656 <tatayousse@gmail.com>
2025-06-22 19:21:08 +0100

    hjxd

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-
 // vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/projet2/', // EXACTEMENT le nom de ton dépôt GitHub
+  base: '/projet2/', // ← CHEMIN CORRECT POUR GITHUB PAGES
   plugins: [react()],
 })
