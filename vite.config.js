7934b0dbc526e6f298b7d92a9aa2a5c7e13a9eaa
youssef9656 <tatayousse@gmail.com>
2025-06-22 18:10:09 +0100

    cv

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/projet2/', // ← nom de ton repo GitHub
+});
